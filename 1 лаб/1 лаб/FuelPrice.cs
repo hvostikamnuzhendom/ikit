f5d8eb1a89f620ea3da715ffd891443ec8866cd9
hvostikamnuzhendom <ajyynapopova98@gmail.com>
2026-09-09 21:35:04 +0700

    добавил мой код

---
+++ 1 лаб/1 лаб/FuelPrice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _1_лаб
+{
+    public class FuelPrice
+    {
+        public string FuelType { get; private set; }
+        public DateTime Date { get; private set; }
+        public double Price { get; private set; }
+
+        public FuelPrice(string fuelType, DateTime date, double price)
+        {
+            FuelType = fuelType;
+            Date = date;
+            Price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Объект: Цена топлива");
+            Console.WriteLine($"  Тип топлива: {FuelType}");
+            Console.WriteLine($"  Дата:        {Date:yyyy.MM.dd}");
+            Console.WriteLine($"  Цена:        {Price:F2} руб.");
+        }
+
+        public static FuelPrice ParseFromString(string input)
+        {
+            var match = Regex.Match(input, @"^\S+\s+""([^""]+)""\s+(\d{4}\.\d{2}\.\d{2})\s+(\d+[\.,]\d+)");
+
+            if (match.Success)
+            {
+                string fuelType = match.Groups[1].Value;
+
+                DateTime date = DateTime.ParseExact(match.Groups[2].Value, "yyyy.MM.dd", CultureInfo.InvariantCulture);
+
+                string priceStr = match.Groups[3].Value.Replace('.', ',');
+                double price = double.Parse(priceStr, new CultureInfo("ru-RU"));
+
+                return new FuelPrice(fuelType, date, price);
+            }
+
+            throw new ArgumentException("Входная строка не соответствует требуемому формату.");
+        }
+    }
+
+}
---
+++ 1 лаб/1 лаб/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаб
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            List<string> testCases = new List<string>
+            {
+                "Цена_топлива \"АИ-95\" 2026.09.03 54.70",
+                "Цена_топлива    \"ДТ_Зимнее\"    2026.12.15    65.25",
+                "Цена_топлива \"АИ-100\" 2026.05.20 72.00"
+            };
+
+            Console.WriteLine("--- Запуск тестирования программы на C# ---");
+            Console.WriteLine();
+
+            for (int i = 0; i < testCases.Count; i++)
+            {
+                Console.WriteLine($"Входная строка №{i + 1}: {testCases[i]}");
+
+                try
+                {
+                    FuelPrice fp = FuelPrice.ParseFromString(testCases[i]);
+
+                    fp.Print();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при разборе: {ex.Message}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
